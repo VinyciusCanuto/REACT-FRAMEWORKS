f44d62682361e2a8b79c750d1a9455733a16aaf7
Vinycius Canuto <vinycius.canuto018@gmail.com>
2024-05-10 11:46:38 -0300

    Primeiro commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import { Titulo, Titulo2, Container } from "../Styles/app"
+
+const App = () => {
+
+    return(
+        <Container>
+        <Titulo>Hello World!</Titulo>
+        <Titulo2>Olá, mundo!</Titulo2>
+        </Container>
+    )
+}
+
+export default App
+
+
